56136f3ac5b370649003b2116aeab5ee4116c170
김형철 <hckim@inswave.com>
2026-03-23 09:49:09 +0900

    자동화 중 샘플 수정

--- src/main/webapp/websquare/config.js
+++ src/main/webapp/websquare/config.js
@@ -524,7 +524,7 @@ export default {
       "focusCalcSize": {
         "@value": "false"
       },
-      "editFormatParticalMask": {
+      "editFormatPartialMask": {
         "@value": "true"
       }
     },
